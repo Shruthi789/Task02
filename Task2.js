816c6fd5018a527c86e6f7ebeb4890489b84645c
Shruthi789 <86235356+Shruthi789@users.noreply.github.com>
2021-06-24 21:04:17 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,27 @@
+setTimeout(function(){	
+document.getElementById('BirthdayMessage').innerText="00:05";
+},(1000));
+
+setTimeout(function(){	
+document.getElementById('BirthdayMessage').innerText="00:04";
+},(2000));
+
+setTimeout(function(){	
+document.getElementById('BirthdayMessage').innerText="00:03";
+},(3000));
+
+setTimeout(function(){	
+document.getElementById('BirthdayMessage').innerText="00:02";
+},(4000));
+
+setTimeout(function(){	
+document.getElementById('BirthdayMessage').innerText="00:01";
+},(5000));
+
+setTimeout(function(){	
+document.getElementById('BirthdayMessage').innerText="00:00";
+},(6000));
+
+setTimeout(function(){
+document.getElementById('BirthdayMessage').innerText="HAPPY BIRTHDAY!!!";
+},(7000));
